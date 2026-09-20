59b4451c27fde6025ed6aa6ef09abb0dc39f6789
tianchoy <tianchoy@live.com>
2026-09-20 12:42:33 +0800

    更换地图 key

--- unpackage/resources/app-ios/uni_modules/external-map-navigation/utssdk/app-ios/src/index.swift
+++ unpackage/resources/app-ios/uni_modules/external-map-navigation/utssdk/app-ios/src/index.swift
@@ -152,7 +152,7 @@ public func buildTencentURL(_ params: ExternalMapNavigationParams) -> String {
     var longitude = params.longitude.toString() ?? ""
     var url = "qqmap://map/routeplan?type=drive&from=" + origin + "&fromcoord=CurrentLocation&to=" + name
     url += "&tocoord=" + latitude + "," + longitude
-    url += "&referer=WDHBZ-AWV6Q-S6I52-2TE5D-UBMX2-NEBAY"
+    url += "&referer=5VNBZ-M6QCL-VIDPL-E7765-7O6QZ-5QFNG"
     return url
 }
 public func buildAmapURL(_ params: ExternalMapNavigationParams) -> String {
